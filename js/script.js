076b5d358e325d69383531153b7a1ac831e77a1f
Ali58211 <Aliibrahimcanario@gmail.com>
2024-08-29 20:35:30 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* 
+let edad = 10;
+edad=10;
+
+let nombre= Usuario;
+nombre= Usuario;
+
+console.log("Bienvenido" + n)
+
+alert(edad) */
+
+let num1;
+let num2;
+
+
+num1 = 5;
+num2 = 15;
+
+
+
+
